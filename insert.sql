8c37eb57237855b31ec399a95030956fb4786a90
Cyraullie <cyril.goldenschue@gmail.com>
2023-12-27 11:48:37 +0100

    add data on db

---
+++ insert.sql
@@ -0,0 +1,56 @@
+
+
+INSERT INTO group_athletes (name) VALUES ("Lundi");
+INSERT INTO group_athletes (name) VALUES ("Jeudi");
+INSERT INTO group_athletes (name) VALUES ("Vendredi 1");
+INSERT INTO group_athletes (name) VALUES ("Vendredi 2");
+INSERT INTO group_athletes (name) VALUES ("Samedi");
+
+INSERT INTO athletes (firstname) VALUES ("Mailyn");
+INSERT INTO athletes (firstname) VALUES ("Sophie");
+INSERT INTO athletes (firstname) VALUES ("Mila");
+INSERT INTO athletes (firstname) VALUES ("Méline");
+
+INSERT INTO athletes (firstname) VALUES ("Zoé");
+INSERT INTO athletes (firstname) VALUES ("Léana");
+INSERT INTO athletes (firstname) VALUES ("Sara");
+INSERT INTO athletes (firstname) VALUES ("Délia");
+INSERT INTO athletes (firstname) VALUES ("Elise");
+
+INSERT INTO athletes (firstname) VALUES ("Lila");
+INSERT INTO athletes (firstname) VALUES ("Mélissa");
+INSERT INTO athletes (firstname) VALUES ("Lilly");
+INSERT INTO athletes (firstname) VALUES ("Laurine");
+INSERT INTO athletes (firstname) VALUES ("Eléanore");
+
+INSERT INTO athletes (firstname) VALUES ("Aurélie B");
+INSERT INTO athletes (firstname) VALUES ("Mélinda");
+INSERT INTO athletes (firstname) VALUES ("Gabriella");
+INSERT INTO athletes (firstname) VALUES ("Laura M");
+INSERT INTO athletes (firstname) VALUES ("Thays");
+INSERT INTO athletes (firstname) VALUES ("Clémence");
+INSERT INTO athletes (firstname) VALUES ("Kaléa");
+INSERT INTO athletes (firstname) VALUES ("Ylenia");
+INSERT INTO athletes (firstname) VALUES ("Léonie");
+
+INSERT INTO athletes (firstname) VALUES ("Lana");
+INSERT INTO athletes (firstname) VALUES ("Lucie");
+
+INSERT INTO athletes (firstname) VALUES ("Laura C");
+INSERT INTO athletes (firstname) VALUES ("Giulia");
+INSERT INTO athletes (firstname) VALUES ("Julie");
+INSERT INTO athletes (firstname) VALUES ("Mélia");
+INSERT INTO athletes (firstname) VALUES ("Adriana");
+INSERT INTO athletes (firstname) VALUES ("Iris");
+
+INSERT INTO athletes (firstname) VALUES ("Aurélie G");
+
+INSERT INTO group_elements (name, link) VALUES ("V.HW.1", "https://vimeo.com/555994762/7ff98ea832?autoplay=1");
+
+INSERT INTO levels (name, group_elements_id) VALUES ("Rouler MD", 1);
+
+INSERT INTO elements (name, levels_id) VALUES ("Hand Roll R - Forward", 1);
+
+INSERT INTO state_elements (athletes_id, elements_id, state) VALUES (6, 1, "R");
+
+
