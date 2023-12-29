862c889f667a47f03a42aae2b36f572fd386419f
Cyraullie <cyril.goldenschue@gmail.com>
2023-12-29 22:00:58 +0100

    update database

--- init.sql
+++ init.sql
@@ -20,9 +20,8 @@ USE `twirling` ;
 CREATE TABLE IF NOT EXISTS `twirling`.`group_elements` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
-  `link` TEXT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -31,15 +30,15 @@ ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 CREATE TABLE IF NOT EXISTS `twirling`.`levels` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
-  `group_elements_id` INT NOT NULL,
+  `group_element_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_levels_group_elements_idx` (`group_elements_id` ASC) VISIBLE,
+  INDEX `fk_levels_group_elements_idx` (`group_element_id` ASC) VISIBLE,
   CONSTRAINT `fk_levels_group_elements`
-    FOREIGN KEY (`group_elements_id`)
+    FOREIGN KEY (`group_element_id`)
     REFERENCES `twirling`.`group_elements` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION) 
-ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -48,15 +47,16 @@ ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 CREATE TABLE IF NOT EXISTS `twirling`.`elements` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
-  `levels_id` INT NOT NULL,
+  `level_id` INT NOT NULL,
+  `link` TEXT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_elements_levels1_idx` (`levels_id` ASC) VISIBLE,
+  INDEX `fk_elements_levels1_idx` (`level_id` ASC) VISIBLE,
   CONSTRAINT `fk_elements_levels1`
-    FOREIGN KEY (`levels_id`)
+    FOREIGN KEY (`level_id`)
     REFERENCES `twirling`.`levels` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -65,8 +65,8 @@ ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 CREATE TABLE IF NOT EXISTS `twirling`.`group_athletes` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`)) 
-ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -75,45 +75,63 @@ ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 CREATE TABLE IF NOT EXISTS `twirling`.`athletes` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `firstname` VARCHAR(255) NOT NULL,
-  `group_athletes_id1` INT NULL,
-  `group_athletes_id2` INT NULL,
+  `group_athlete_id1` INT NULL,
+  `group_athlete_id2` INT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_athletes_group_athletes1_idx` (`group_athletes_id1` ASC) VISIBLE,
-  INDEX `fk_athletes_group_athletes2_idx` (`group_athletes_id2` ASC) VISIBLE,
+  INDEX `fk_athletes_group_athletes1_idx` (`group_athlete_id1` ASC) VISIBLE,
+  INDEX `fk_athletes_group_athletes2_idx` (`group_athlete_id2` ASC) VISIBLE,
   CONSTRAINT `fk_athletes_group_athletes1`
-    FOREIGN KEY (`group_athletes_id1`)
+    FOREIGN KEY (`group_athlete_id1`)
     REFERENCES `twirling`.`group_athletes` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_athletes_group_athletes2`
-    FOREIGN KEY (`group_athletes_id2`)
+    FOREIGN KEY (`group_athlete_id2`)
     REFERENCES `twirling`.`group_athletes` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `twirling`.`variations`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `twirling`.`variations` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `element_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_variations_elements1_idx` (`element_id` ASC) VISIBLE,
+  CONSTRAINT `fk_variations_elements1`
+    FOREIGN KEY (`element_id`)
+    REFERENCES `twirling`.`elements` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `twirling`.`state_elements`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `twirling`.`state_elements` (
-  `athletes_id` INT NOT NULL,
-  `elements_id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `athlete_id` INT NOT NULL,
+  `variation_id` INT NOT NULL,
   `state` ENUM(' ', 'C', 'R', 'V') NOT NULL DEFAULT ' ',
-  PRIMARY KEY (`athletes_id`, `elements_id`),
-  INDEX `fk_athletes_has_elements_elements1_idx` (`elements_id` ASC) VISIBLE,
-  INDEX `fk_athletes_has_elements_athletes1_idx` (`athletes_id` ASC) VISIBLE,
+  INDEX `fk_athletes_has_elements_athletes1_idx` (`athlete_id` ASC) VISIBLE,
+  INDEX `fk_state_elements_variations1_idx` (`variation_id` ASC) VISIBLE,
+  PRIMARY KEY (`id`),
   CONSTRAINT `fk_athletes_has_elements_athletes1`
-    FOREIGN KEY (`athletes_id`)
+    FOREIGN KEY (`athlete_id`)
     REFERENCES `twirling`.`athletes` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_athletes_has_elements_elements1`
-    FOREIGN KEY (`elements_id`)
-    REFERENCES `twirling`.`elements` (`id`)
+  CONSTRAINT `fk_state_elements_variations1`
+    FOREIGN KEY (`variation_id`)
+    REFERENCES `twirling`.`variations` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -124,8 +142,9 @@ CREATE TABLE IF NOT EXISTS `twirling`.`musics` (
   `name` VARCHAR(255) NOT NULL,
   `extension` VARCHAR(255) NOT NULL,
   `filename` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`)) 
-ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
--- insert.sql
+++ insert.sql
@@ -6,51 +6,68 @@ INSERT INTO group_athletes (name) VALUES ("Vendredi 1");
 INSERT INTO group_athletes (name) VALUES ("Vendredi 2");
 INSERT INTO group_athletes (name) VALUES ("Samedi");
 
-INSERT INTO athletes (firstname) VALUES ("Mailyn");
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Mailyn", 1);
 INSERT INTO athletes (firstname) VALUES ("Sophie");
 INSERT INTO athletes (firstname) VALUES ("Mila");
-INSERT INTO athletes (firstname) VALUES ("Méline");
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Méline", 1);
 
-INSERT INTO athletes (firstname) VALUES ("Zoé");
-INSERT INTO athletes (firstname) VALUES ("Léana");
-INSERT INTO athletes (firstname) VALUES ("Sara");
-INSERT INTO athletes (firstname) VALUES ("Délia");
-INSERT INTO athletes (firstname) VALUES ("Elise");
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Zoé", 1);
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Léana", 1);
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Sara", 1);
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Délia", 1);
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Elise", 1);
 
-INSERT INTO athletes (firstname) VALUES ("Lila");
-INSERT INTO athletes (firstname) VALUES ("Mélissa");
-INSERT INTO athletes (firstname) VALUES ("Lilly");
-INSERT INTO athletes (firstname) VALUES ("Laurine");
-INSERT INTO athletes (firstname) VALUES ("Eléanore");
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Lila", 1, 2);
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Mélissa", 1, 2);
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Lilly", 1, 2);
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Laurine", 1, 2);
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Eléanore", 1, 2);
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Annie", 1);
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Mila", 1);
 
-INSERT INTO athletes (firstname) VALUES ("Aurélie B");
-INSERT INTO athletes (firstname) VALUES ("Mélinda");
-INSERT INTO athletes (firstname) VALUES ("Gabriella");
-INSERT INTO athletes (firstname) VALUES ("Laura M");
-INSERT INTO athletes (firstname) VALUES ("Thays");
-INSERT INTO athletes (firstname) VALUES ("Clémence");
-INSERT INTO athletes (firstname) VALUES ("Kaléa");
-INSERT INTO athletes (firstname) VALUES ("Ylenia");
-INSERT INTO athletes (firstname) VALUES ("Léonie");
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Aurélie B", 3, 4);
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Mélinda", 2, 3);
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Gabriella", 3, 4);
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Laura M", 3, 4);
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Thays", 2, 3);
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Clémence", 2, 3);
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Kaléa", 3, 4);
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Ylenia", 3, 4);
+INSERT INTO athletes (firstname, group_athlete_id1, group_athlete_id2) VALUES ("Léonie", 3, 4);
 
-INSERT INTO athletes (firstname) VALUES ("Lana");
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Lana", 5);
 INSERT INTO athletes (firstname) VALUES ("Lucie");
 
-INSERT INTO athletes (firstname) VALUES ("Laura C");
-INSERT INTO athletes (firstname) VALUES ("Giulia");
-INSERT INTO athletes (firstname) VALUES ("Julie");
-INSERT INTO athletes (firstname) VALUES ("Mélia");
-INSERT INTO athletes (firstname) VALUES ("Adriana");
-INSERT INTO athletes (firstname) VALUES ("Iris");
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Laura C", 5);
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Giulia", 5);
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Julie", 5);
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Mélia", 5);
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Adriana", 5);
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Iris", 5);
 
-INSERT INTO athletes (firstname) VALUES ("Aurélie G");
+INSERT INTO athletes (firstname, group_athlete_id1) VALUES ("Aurélie G", 5);
 
-INSERT INTO group_elements (name, link) VALUES ("V.HW.1", "https://vimeo.com/555994762/7ff98ea832?autoplay=1");
+INSERT INTO group_elements (name) VALUES ("Vertical Roll");
+INSERT INTO group_elements (name) VALUES ("Horizontal Roll");
 
-INSERT INTO levels (name, group_elements_id) VALUES ("Rouler MD", 1);
+INSERT INTO levels (name, group_element_id) VALUES ("Foundation", 1);
+INSERT INTO levels (name, group_element_id) VALUES ("Groupe A", 1);
+INSERT INTO levels (name, group_element_id) VALUES ("Groupe B", 1);
+INSERT INTO levels (name, group_element_id) VALUES ("Groupe C", 1);
+INSERT INTO levels (name, group_element_id) VALUES ("Groupe D", 1);
+INSERT INTO levels (name, group_element_id) VALUES ("Groupe E", 1);
+INSERT INTO levels (name, group_element_id) VALUES ("Foundation", 2);
 
-INSERT INTO elements (name, levels_id) VALUES ("Hand Roll R - Forward", 1);
+INSERT INTO elements (name, link, level_id) VALUES ("Hand Roll", "https://vimeo.com/555994762/7ff98ea832?autoplay=1", 1);
+INSERT INTO elements (name, link, level_id) VALUES ("Paddle", "https://vimeo.com/555994836/a9390ccc6d?autoplay=1", 1);
 
-INSERT INTO state_elements (athletes_id, elements_id, state) VALUES (6, 1, "R");
+INSERT INTO variations (name, element_id) VALUES ("Hand Roll R - Forward", 1);
+INSERT INTO variations (name, element_id) VALUES ("Hand Roll L - Forward", 1);
+
+INSERT INTO variations (name, element_id) VALUES ("Paddle Forward", 2);
+INSERT INTO variations (name, element_id) VALUES ("Paddle Reverse", 2);
+
+INSERT INTO state_elements (athlete_id, variation_id, state) VALUES (6, 1, "R");
+INSERT INTO state_elements (athlete_id, variation_id, state) VALUES (6, 2, " ");
 
 
