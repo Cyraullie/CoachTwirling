8ec114195787555714c50752d42222eefa820ad3
Cyraullie <cyril.goldenschue@gmail.com>
2023-12-30 22:38:13 +0100

    Add route to update state_element

--- api/routes/api.php
+++ api/routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\AthleteController;
 use App\Http\Controllers\GroupAthleteController;
 use App\Http\Controllers\MusicController;
 use App\Http\Controllers\VariationController;
+use App\Http\Controllers\StateElementController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +34,7 @@ Route::get("/group_elements", [GroupElementController::class, "show_all"]);
 Route::get("/group_elements/{group_elements_id}/levels", [LevelController::class, "show_all"]);
 Route::get("/group_elements/{group_elements_id}/levels/{levels_id}/elements", [ElementController::class, "show_all"]);
 Route::get("/group_elements/{group_elements_id}/levels/{levels_id}/elements/{elements_id}/variations", [VariationController::class, "show_all"]);
+Route::post("/state_element", [StateElementController::class, "update"]);
 
 
 
